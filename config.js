58f239a55f29c9cae02036314b1a21ce284ca6c2
Mike Wilcox <anm9tr@gmail.com>
2018-08-05 09:38:37 -0500

    find repo parent

--- config.js
+++ config.js
@@ -1,12 +1,34 @@
 const path = require('path');
 
 module.exports = {
-	root: '/sites/github',
-	dependencies: [
-		{ 'undo-stack': [
-				{
-					'proxify': []
-				}
-			]}
-	]
+	root: '/sites',
+	repos: {
+		'test': {
+			'./github/undo-stack': {
+				'./github/foo': { './github/foo-child': {} },
+				'./github/bar': {
+					'./github/bar-child': {
+						'./github/bar-grand-child': {}
+					}
+				},
+				'./github/proxify': { './github/foo-child': {} },
+			}
+		}
+	}
 };
+
+// module.exports = {
+// 	root: '/sites/github',
+// 	repos: {
+// 		'undo-stack': {
+// 			'foo': { 'foo-child': {} },
+// 			'bar': {
+// 				'bar-child': {
+// 					'bar-grand-child': {}
+// 				}
+// 			},
+// 			'proxify': { 'foo-child': {} },
+// 		}
+// 	}
+// };
+//
--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const nav = require('./lib/nav');
 const git = require('./lib/git');
 
 nav.pkg();
-git.commit('test commit').then(git.push);
+nav.parent();
+nav.pkg();
+nav.parent();
+nav.pkg();
+
+// git.commit('test commit').then(git.push);
